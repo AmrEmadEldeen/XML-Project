ad9ca441d9ed7070a3eef87a54575704616c380c
AmrEmadEldeen <85315099+AmrEmadEldeen@users.noreply.github.com>
2021-12-21 08:02:16 +0200

    Update MainGui.h

--- XmlParser/XmlParser/MainGui.h
+++ XmlParser/XmlParser/MainGui.h
@@ -370,4 +370,68 @@ while (!T.empty())
   }
 }
 };
+private: static bool findChar(char x, string y) {
+    for (unsigned int i = 0; i < y.length(); i++) {
+		if (y[i] == x) {
+			return 1;
+		}
+	}
+	return 0;
+}
+private: static string formatting(vector<string> t) {
+	int ind = 0;
+	int openflag = 0;
+	int closeflag = 0;
+	string str = "";
+	for (unsigned int i = 0; i < t.size(); i++) {
+		if (t[i][0] == '<' && (t[i][1] == '?' || t[i][1] == '!')) {
+			str += t[i] + "\n";		//print output
+		}
+		else if (t[i][0] == '<' && !findChar('/', t[i])) {
+			openflag++;
+			closeflag = 0;
+			if (openflag > 1) {
+				ind++;
+			}
+			for (int j = 0; j < ind; j++) {
+				str += "    ";		//print tab
+			}
+			str += t[i] + "\n";	//print output
+		}
+		else if (t[i][0] == '<' && t[i][1] == '/') {
+			closeflag++;
+			openflag = 0;
+			if (closeflag > 1) {
+				ind--;
+			}
+			for (int j = 0; j < ind; j++) {
+				str += "    ";		//print tab
+			}
+			str += t[i] + "\n";	//print output
+		}
+		else if (t[i][0] == '<' && findChar('/', t[i])) {
+			openflag++;
+			closeflag = 0;
+			if (openflag > 1) {
+				ind++;
+			}
+			for (int j = 0; j < ind; j++) {
+				str += "    ";		//print tab
+			}
+			str += t[i] + "\n";	//print output
+			closeflag++;
+			openflag = 0;
+			if (closeflag > 1) {
+				ind--;
+			}
+		}
+		else {
+			for (int j = 0; j < ind; j++) {
+				str += "    ";		//print tab
+			}
+			str += t[i] + "\n";	//print output
+		}
+	}
+	return str;
+}
 }
